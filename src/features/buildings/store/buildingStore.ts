68b9d7de5d572d93e21441d7173b2bf32bc83613
Tobias Moreno <tobias.moreno@techforb.com>
2025-05-20 14:11:12 -0300

    CHG: se eliminan los mensajes de consola en el manejo de errores en el store de edificios, simplificando el código y mejorando la limpieza de la salida de errores.

--- src/features/buildings/store/buildingStore.ts
+++ src/features/buildings/store/buildingStore.ts
@@ -26,14 +26,11 @@ export const useBuildingStore = defineStore("building", {
             ([_, v]) => v !== undefined && v !== ""
           )
         );
-        console.log("Filtros enviados:", clean);
 
         const { data } = await api.post<Building[]>("/buildings/filter", clean);
         this.buildings = data;
       } catch (err: any) {
         this.error = "Error filtering buildings";
-        console.error(err);
-        console.error("Detalle del error:", err.response?.data);
       } finally {
         this.isLoading = false;
       }
